85823aad1945cad29c5e2c11fdaad1669716ba90
Bhagyesh-20 <bhagyesh.dh20@gmail.com>
2024-06-26 21:39:46 +0530

    ,,

--- Hello/wsgi.py
+++ Hello/wsgi.py
@@ -7,13 +7,23 @@ For more information on this file, see
 https://docs.djangoproject.com/en/4.2/howto/deployment/wsgi/
 """
 
-import os
 
+
+import sys
+print("Python Path:", sys.path)
+
+"""
+WSGI config for Hello project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.2/howto/deployment/wsgi/
+"""
+
+import os
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Hello.settings')
 
 application = get_wsgi_application()
-
-app = application
-
