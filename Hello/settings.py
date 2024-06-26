48886987f7e8905eeb9cfc5c63230500b54b142f
Bhagyesh-20 <bhagyesh.dh20@gmail.com>
2024-06-26 21:09:17 +0530

    ..

--- Hello/settings.py
+++ Hello/settings.py
@@ -80,19 +80,19 @@ WSGI_APPLICATION = 'Hello.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 # Replace the SQLite DATABASES configuration with PostgreSQL:
-DATABASES = {
-    'default': dj_database_url.config(
-        # Replace this value with your local database's connection string.
-        default='postgresql://postgres:postgres@localhost:5432/mysite',
-        conn_max_age=600
-    )
-}
 # DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
+#     'default': dj_database_url.config(
+#         # Replace this value with your local database's connection string.
+#         default='postgresql://postgres:postgres@localhost:5432/mysite',
+#         conn_max_age=600
+#     )
 # }
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
--- home/migrations/0007_alter_order_options_remove_order_id_order_order_id.py
+++ home/migrations/0007_alter_order_options_remove_order_id_order_order_id.py
@@ -21,7 +21,6 @@ class Migration(migrations.Migration):
         migrations.AddField(
             model_name='order',
             name='order_id',
-            field=models.AutoField(default=1, primary_key=True, serialize=False),
-            preserve_default=False,
+            field=models.AutoField(primary_key=True, serialize=False),
         ),
     ]
